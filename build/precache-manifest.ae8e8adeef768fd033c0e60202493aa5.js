5cd4298165b05160d944fd6a3af8a047fe87f80d
ShubbhamJain <shubham.jain368@gmail.com>
2020-06-09 19:17:40 +0530

    commiting build

---
+++ build/precache-manifest.ae8e8adeef768fd033c0e60202493aa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc60fde52bc664d821a4341a6cdb3842",
+    "url": "./index.html"
+  },
+  {
+    "revision": "324c53a83cbdd65db095",
+    "url": "./static/css/main.e64fa0e5.chunk.css"
+  },
+  {
+    "revision": "4139184cc9ebb59aeb5e",
+    "url": "./static/js/2.3627c797.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.3627c797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "324c53a83cbdd65db095",
+    "url": "./static/js/main.c330834b.chunk.js"
+  },
+  {
+    "revision": "d334f552f08816a4f83a",
+    "url": "./static/js/runtime-main.1ec3e5f4.js"
+  }
+]);
